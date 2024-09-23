6afe491793043814f16f43d3d37021bc4c605876
Minwoo P <alsd2013@kakao.com>
2024-09-23 07:47:03 +0000

    dashboard CQRS 로직 추가 및 dashboard 서비스에 대한 gateway path 추가

--- dashboard/src/main/java/library/infra/UserInfoViewHandler.java
+++ dashboard/src/main/java/library/infra/UserInfoViewHandler.java
@@ -21,22 +21,33 @@ public class UserInfoViewHandler {
     public void whenBookBorrowed_then_CREATE_1(
         @Payload BookBorrowed bookBorrowed
     ) {
-        try {
-            if (!bookBorrowed.validate()) return;
+    try {
+        if (!bookBorrowed.validate()) return;
 
-            // view 객체 생성
+        // view 객체 조회
+        Optional<UserInfo> userInfoOptional = userInfoRepository.findById(
+            Long.valueOf(bookBorrowed.getUserId())
+        );
+
+        if (userInfoOptional.isPresent()) {
+            // 기존 객체가 있을 경우 borrowCnt 증가
+            UserInfo userInfo = userInfoOptional.get();
+            userInfo.setBorrowCnt(userInfo.getBorrowCnt() + bookBorrowed.getQty());
+            userInfoRepository.save(userInfo);
+        } else {
+            // 객체가 없을 경우 새로 생성하여 저장
             UserInfo userInfo = new UserInfo();
-            // view 객체에 이벤트의 Value 를 set 함
             userInfo.setUserId(Long.valueOf(bookBorrowed.getUserId()));
             userInfo.setBorrowCnt(bookBorrowed.getQty());
             userInfo.setExpireFee(0);
-            // view 레파지 토리에 save
             userInfoRepository.save(userInfo);
-        } catch (Exception e) {
-            e.printStackTrace();
         }
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
     }
 
+    // 반납 시, 대출 중인 책 수량을 감소
     @StreamListener(KafkaProcessor.INPUT)
     public void whenBookReturned_then_UPDATE_1(
         @Payload BookReturned bookReturned
@@ -44,7 +55,7 @@ public class UserInfoViewHandler {
         try {
             if (!bookReturned.validate()) return;
             // view 객체 조회
-            Optional<UserInfo> userInfoOptional = userInfoRepository.findByUserId(
+            Optional<UserInfo> userInfoOptional = userInfoRepository.findById(
                 Long.valueOf(bookReturned.getUserId())
             );
 
@@ -62,6 +73,7 @@ public class UserInfoViewHandler {
         }
     }
 
+    // 연체 시, 연체료를 추가
     @StreamListener(KafkaProcessor.INPUT)
     public void whenBookExpired_then_UPDATE_2(
         @Payload BookExpired bookExpired
@@ -69,14 +81,30 @@ public class UserInfoViewHandler {
         try {
             if (!bookExpired.validate()) return;
             // view 객체 조회
-            Optional<UserInfo> userInfoOptional = userInfoRepository.findByUserId(
+            Optional<UserInfo> userInfoOptional = userInfoRepository.findById(
                 Long.valueOf(bookExpired.getUserId())
             );
+            
 
             if (userInfoOptional.isPresent()) {
                 UserInfo userInfo = userInfoOptional.get();
-                // view 객체에 이벤트의 eventDirectValue 를 set 함
-                userInfo.setExpireFee(userInfo.getExpireFee() + 500);
+
+                // 연체 기간에 따른 벌금 계산
+                int expireDays = bookExpired.getExpireDays();
+                int fee = 0;
+
+                if (expireDays >= 1 && expireDays <= 7) {
+                    fee = 500;
+                } else if (expireDays >= 8 && expireDays <= 14) {
+                    fee = 1000;
+                } else if (expireDays > 14) {
+                    fee = 2000;
+                }
+
+
+                //  // 벌금을 현재 값에 더함
+                userInfo.setExpireFee(userInfo.getExpireFee() + fee);
+
                 // view 레파지 토리에 save
                 userInfoRepository.save(userInfo);
             }
